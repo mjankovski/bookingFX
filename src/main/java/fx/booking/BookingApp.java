d1ab6fb9fdb99f6b11bfc2d8b3941a5cd0bff6e9
Miłosz Jankowski <miloszjankowski11@gmail.com>
2018-12-05 21:27:11 +0100

    Getting dbconfig from .properties.

--- src/main/java/fx/booking/BookingApp.java
+++ src/main/java/fx/booking/BookingApp.java
@@ -39,7 +39,4 @@ public class BookingApp extends Application{
         public void stop() {
             springContext.stop();
         }
-
-
-
     }
--- src/main/java/fx/booking/SpringJDBCConfiguration.java
+++ src/main/java/fx/booking/SpringJDBCConfiguration.java
@@ -1,27 +1,32 @@
 package fx.booking;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan
+@PropertySource("classpath:dbconfig.properties")
 public class SpringJDBCConfiguration {
+
+    @Autowired
+    private Environment env;
 
     @Bean
     public DataSource dataSource() {
         MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setDatabaseName("");
-        dataSource.setServerName("");
-        dataSource.setUser("");
-        dataSource.setPassword("");
+        dataSource.setDatabaseName(env.getProperty("db.name"));
+        dataSource.setServerName(env.getProperty("db.servername"));
+        dataSource.setUser(env.getProperty("db.user"));
+        dataSource.setPassword(env.getProperty("db.password"));
 
         return dataSource;
     }
