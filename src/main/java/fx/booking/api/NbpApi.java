11c8b7517ffd0f0109f68932366c0c9d7ce03957
mjankovski <miloszjankowski11@gmail.com>
2019-01-14 21:23:25 +0100

    NbpApi exception handling

--- src/main/java/fx/booking/api/NbpApi.java
+++ src/main/java/fx/booking/api/NbpApi.java
@@ -6,24 +6,40 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
 public class NbpApi {
-    private HttpResponse<JsonNode> jsonResponse;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public BigDecimal getPlnToEuroCurrency(){
         try {
-            jsonResponse = Unirest.get("http://api.nbp.pl/api/exchangerates/rates/C/EUR")
+            HttpResponse<JsonNode> jsonResponse = Unirest
+                    .get("http://api.nbp.pl/api/exchangerates/rates/C/EUR")
                     .asJson();
-        } catch (
-                UnirestException e) {
-            e.printStackTrace();
-        }
+            JSONArray rates = (JSONArray) jsonResponse
+                    .getBody()
+                    .getObject()
+                    .get("rates");
 
-        JSONArray rates = (JSONArray) jsonResponse.getBody().getObject().get("rates");
-        return new BigDecimal((double)((JSONObject) rates.get(0)).get("bid"));
+            BigDecimal currency = new BigDecimal((double)((JSONObject) rates
+                    .get(0))
+                    .get("bid"));
+
+            jdbcTemplate.update(
+                    "UPDATE Kursy " +
+                    "SET KURS = ?, DATA_KURSU = ?" +
+                    "WHERE WALUTA = ?"
+                    , currency, java.time.LocalDate.now(), "EUR");
+
+            return currency;
+        } catch (UnirestException e) {
+            return jdbcTemplate.queryForObject("SELECT KURS FROM Kursy WHERE WALUTA = ?", BigDecimal.class, "EUR");
+        }
     }
 }
